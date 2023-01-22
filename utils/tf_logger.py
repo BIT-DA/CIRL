77d40d2261a60e917d96dc7b8b9217ad984b7502
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-01-23 02:22:39 +0330

    tensorflow functions updated

--- utils/tf_logger.py
+++ utils/tf_logger.py
@@ -13,12 +13,18 @@ class TFLogger(object):
 
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
-        self.writer = tf.summary.FileWriter(log_dir)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        #self.writer.add_summary(summary, step)
+        
+        with self.writer.as_default():
+
+              tf.summary.scalar(tag, value, step=step)
+
+              self.writer.flush()
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
