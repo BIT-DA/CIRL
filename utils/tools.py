82be03a22874c4d1b6876b5b53c5607b8fc4e382
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-01-26 17:30:36 +0330

    KL changed 3

--- utils/tools.py
+++ utils/tools.py
@@ -74,10 +74,10 @@ def factorization_loss(f_a, f_b):
     #f_a_norm = f_a
     #f_b_norm = f_b
 
-    element_wise = 0.5 * (0 - torch.log(f_a.std(0)) + f_a.std(0) / 1 + (f_a.mean(0) - 0).pow(2) / 1 - 1)
+    element_wise = 0.5 * (0 - torch.log(f_a.std()) + f_a.std() / 1 + (f_a.mean() - 0).pow(2) / 1 - 1)
     kl_1 = element_wise.sum(-1)
 
-    element_wise_ = 0.5 * (0 - torch.log(f_b.std(0)) + f_b.std(0) / 1 + (f_b.mean(0) - 0).pow(2) / 1 - 1)
+    element_wise_ = 0.5 * (0 - torch.log(f_b.std()) + f_b.std() / 1 + (f_b.mean() - 0).pow(2) / 1 - 1)
     kl_2 = element_wise_.sum(-1)
     ###return kl
 
